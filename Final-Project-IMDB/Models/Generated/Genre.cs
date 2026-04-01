db0883ef9879c1fddf63b18cbc8e5a5d9d03f92a
liammatheson <W0494713@nscc.ca>
2026-04-01 12:42:36 -0300

    scaffolding done

---
+++ Final-Project-IMDB/Models/Generated/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Final_Project_IMDB.Models.Generated;
+
+public partial class Genre
+{
+    public int GenreId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Title> Titles { get; set; } = new List<Title>();
+}
